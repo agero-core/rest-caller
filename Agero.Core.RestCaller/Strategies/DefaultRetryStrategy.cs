1acdee9b91317080f3a84b8c69c70c6632e62377
Jason Penniman <jpenniman@agero.com>
2019-01-29 15:28:22 -0500

    Transient Retry Strategy
    Extracted the transient retry strategy logic out of RESTCaller into it's own class and interface. This allows consumers to define their own logic for what errors they consider transient.
    
    The DefaultRetryStrategy is used if none is provided. There are no braking changes with this update.

---
+++ Agero.Core.RestCaller/Strategies/DefaultRetryStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Agero.Core.RestCaller.Strategies
+{
+    /// <summary>
+    /// Default strategy for detecting transient failures.
+    /// </summary>
+    /// <remarks>
+    /// The strategy considers the following to be transient and retriable:
+    ///    WebExceptionStatus.ConnectFailure,
+    ///    WebExceptionStatus.ConnectionClosed,
+    ///    WebExceptionStatus.NameResolutionFailure,
+    ///    WebExceptionStatus.PipelineFailure,
+    ///    WebExceptionStatus.ReceiveFailure,
+    ///    WebExceptionStatus.SendFailure,
+    ///    WebExceptionStatus.Timeout
+    /// </remarks>
+    public class DefaultRetryStrategy : IRetryStrategy
+    {
+        private static readonly IReadOnlyCollection<WebExceptionStatus> _transientWebExceptionStatuses =
+            new[]
+            {
+                WebExceptionStatus.ConnectFailure,
+                WebExceptionStatus.ConnectionClosed,
+                WebExceptionStatus.NameResolutionFailure,
+                WebExceptionStatus.PipelineFailure,
+                WebExceptionStatus.ReceiveFailure,
+                WebExceptionStatus.SendFailure,
+                WebExceptionStatus.Timeout
+            };
+
+        /// <inheritDoc />
+        public bool IsTransient(WebException webException)
+        {
+            return _transientWebExceptionStatuses.Contains(webException.Status);
+        }
+    }
+}
---
+++ Agero.Core.RestCaller/Strategies/IRetryStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Agero.Core.RestCaller.Strategies
+{
+    /// <summary>
+    /// Interface implemented by retry strategies.
+    /// </summary>
+    public interface IRetryStrategy
+    {
+        /// <summary>
+        /// Determines whether or not the error is transient.
+        /// </summary>
+        /// <param name="webException">The <see cref="WebException"/> to apply the strategy too.</param>
+        /// <returns><c>true</c> if the error is transient, otherwise <c>false</c>.</returns>
+        bool IsTransient(WebException webException);
+    }
+}
